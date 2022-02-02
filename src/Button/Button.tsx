3bbf8b4ce39c0eece0e08e1428b32c8152b3502d
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-02 13:26:04 +0300

    Create Button component

---
+++ src/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  handler: () => {};
+}
+
+export default function Button(props: ButtonProps) {
+  const { text, handler } = props;
+  return (
+    <div className="button__container">
+      <button className="button" onClick={handler}>
+        {text}
+      </button>
+    </div>
+  );
+}
+
+
