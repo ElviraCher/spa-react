0c9a2c2fa3a6283d83d0ab972ecde7c458e63b28
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-03 12:57:01 +0300

    Add email validation

--- src/UserProfile/UserProfile.tsx
+++ src/UserProfile/UserProfile.tsx
@@ -16,29 +16,46 @@ export default function UserProfile(props: UserProfileProps) {
   const [name, setName] = useState(user.name);
   const [userName, setUserName] = useState(user.username);
   const [email, setEmail] = useState(user.email);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Error");
   const [street, setStreet] = useState(user.address.street);
   const [city, setCity] = useState(user.address.city);
   const [zipCode, setZipCode] = useState(user.address.zipcode);
   const [phone, setPhone] = useState(user.phone);
   const [website, setWebsite] = useState(user.website);
   const [comment, setComment] = useState("");
+  const [formValid, setFormValid] = useState(true);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re = /.+@.+\..+/i;
+    if (!re.test(e.target.value)) {
+      setEmailError("Invalid Email");
+      setFormValid(false);
+    } else {
+      setEmailError("");
+      setFormValid(true);
+    }
+  };
 
   const editButtonHandler = () => {
     setEdit(true);
   };
 
   const handleSubmit = (e: React.FormEvent) => {
-    console.log(
-      name,
-      userName,
-      email,
-      street,
-      city,
-      zipCode,
-      phone,
-      website,
-      comment
-    );
+    if (formValid && edit) {
+      console.log(
+        name,
+        userName,
+        email,
+        street,
+        city,
+        zipCode,
+        phone,
+        website,
+        comment
+      );
+    }
     e.preventDefault();
   };
 
@@ -52,7 +69,7 @@ export default function UserProfile(props: UserProfileProps) {
       </button>
       <button
         className={`profile__button profile__button--submit profile__button--submit--${
-          edit ? "active" : "inactive"
+          edit && formValid ? "active" : "inactive"
         }`}
         onClick={handleSubmit}
       >
@@ -76,12 +93,14 @@ export default function UserProfile(props: UserProfileProps) {
           onChange={(e) => setUserName(e.target.value)}
         />
         <p className="form__text">E-mail</p>
+        {emailDirty && emailError && <div>{emailError}</div>}
         <input
+          onBlur={() => setEmailDirty(true)}
+          onChange={(e) => emailHandler(e)}
           readOnly={!edit}
           className={`form__input form__input--${edit ? "active" : "inactive"}`}
           type="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
         />
         <p className="form__text">Street</p>
         <input
