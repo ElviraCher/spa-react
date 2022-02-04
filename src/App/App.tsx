05416cea3ccdab4d09fdb15238041abc4e1b0bc2
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-04 19:07:33 +0300

    Edit App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import UsersList from "../UsersList/UsersList.tsx";
+import UserProfile from "../UserProfile/UserProfile.tsx";
+import Button from "../Button/Button.tsx";
+import Layout from "../Layout/Layout.tsx";
+import URL from "../URL/URL.tsx";
 
-import ReactDOM from "react-dom";
-import { Example } from "../Example/Example.tsx";
+import "./App.css";
 
-function App() {
-  return <Example text="Hello!" />;
+export default function App() {
+  const [usersInfo, setUsersInfo] = useState([]);
+  const [headerText, setHeaderText] = useState("Список пользователей");
+  const [sortWay, setSortWay] = useState("none");
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((users) => setUsersInfo(users));
+  }, []);
+
+  const sortByNameHandler = () => {
+    setSortWay("name");
+  };
+
+  const sortByCityHandler = () => {
+    setSortWay("city");
+  };
+
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <div className="sidebar__nav">
+          <p className="sidebar__header">Сортировка</p>
+          <Button handler={sortByCityHandler} text="по городу" />
+          <Button handler={sortByNameHandler} text="по имени" />
+        </div>
+
+      </div>
+
+      <Routes>
+        <Route path="/*" element={<Layout header={headerText} />}>
+          <Route
+            index
+            element={<UsersList data={usersInfo} sortWay={sortWay} />}
+          />
+          <Route path=":id" element={<UserProfile data={usersInfo} />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
-
-ReactDOM.render(<App />, document.getElementById("app"));
